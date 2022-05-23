d143ec5785f4dd3e924ca9241f335768606548d8
SR Kukadia <87287434+kukadia@users.noreply.github.com>
2022-05-22 23:59:23 -0700

    resolved tests

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello Worlds");
+    expect(hello.hello()).toEqual("Hello World");
   });
 });
